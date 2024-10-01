e534f1e3f5bac2306198ec88450dc8ddbf507db4
Raquel de Sá <silva.raquel.de.sa@gmail.com>
2024-10-01 18:18:08 -0400

    feat: add .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from './app.service';
 import { CountryModule } from './country/country.module';
 import { HttpModule } from '@nestjs/axios';
 import { CountryService } from './country/country.service';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [CountryModule, HttpModule],
+  imports: [CountryModule, HttpModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, CountryService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
-  console.log('Server running on port 5000');
+
+  // Acesse o ConfigService para obter o valor da porta
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT_BACKEND') || 5000; // Porta padrão caso não esteja definida
+
+  await app.listen(port);
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
